a57f6140124b223b2b8e23fdb20c55e9add310ef
aimvelocity <74454528+aimvelocity@users.noreply.github.com>
2021-09-17 23:12:43 -0400

    Update MassUnaddwdmer.py

--- Discord Tool/MassUnaddwdmer.py
+++ Discord Tool/MassUnaddwdmer.py
@@ -15,9 +15,9 @@ async def on_connect():
                 await user.send(f'{user.mention} unadding everyone re-add me')
                 time.sleep(1)
                 await user.remove_friend()
-                print(f"{Fore.LIGHTBLACK_EX}[{Fore.CYAN}!{Fore.LIGHTBLACK_EX}] {Fore.CYAN}Messaged/Unadded: {user}")
+                print(f'{Fore.LIGHTBLACK_EX}[{Fore.CYAN}!{Fore.LIGHTBLACK_EX}] {Fore.CYAN}Messaged/Unadded: {user}')
             except:
-                    print(f"{Fore.LIGHTBLACK_EX}[{Fore.CYAN}!{Fore.LIGHTBLACK_EX}] {Fore.CYAN}couldnt message/unadd: {user}")
+                    print(f'{Fore.LIGHTBLACK_EX}[{Fore.CYAN}!{Fore.LIGHTBLACK_EX}] {Fore.CYAN}couldnt message/unadd: {user}')
 
 
 client.run(token, bot = False)
